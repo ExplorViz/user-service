7366c5da562e9e6071c72e854336ee7469f3981b
Kevin Lotz <lotzk@outlook.com>
2020-10-06 12:32:53 +0200

    Fix missing setters

--- src/main/java/net/explorviz/token/model/LandscapeToken.java
+++ src/main/java/net/explorviz/token/model/LandscapeToken.java
@@ -1,36 +1,53 @@
 package net.explorviz.token.model;
 
 import com.google.common.base.Objects;
+import io.quarkus.mongodb.panache.MongoEntity;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 
 /**
  * Represents a landscape token.
  */
+@MongoEntity
 public class LandscapeToken {
 
   /**
    * The actual token value.
    */
-  @BsonProperty("value")
-  final private String value;
+
+  private String value;
 
   /**
    * The id of the user owning this token.
    */
-  @BsonProperty("owner")
-  final private String ownerId;
+
+  private String ownerId;
 
   public LandscapeToken(final String value, final String ownerId) {
     this.value = value;
     this.ownerId = ownerId;
   }
 
+  public LandscapeToken() { /*Jackson*/ }
+
+
+  @BsonProperty("value")
   public String getValue() {
     return value;
   }
 
+  @BsonProperty("value")
+  public void setValue(final String value) {
+    this.value = value;
+  }
+
+  @BsonProperty("owner")
   public String getOwnerId() {
     return ownerId;
+  }
+
+  @BsonProperty("owner")
+  public void setOwnerId(final String ownerId) {
+    this.ownerId = ownerId;
   }
 
   @Override
