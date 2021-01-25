eaae504d07865ca113712304be8e2991292b92be
Alexander Krause <akr@informatik.uni-kiel.de>
2021-01-25 09:43:19 +0100

    fixed last checkstyle warning

--- src/main/java/net/explorviz/token/service/messaging/EventService.java
+++ src/main/java/net/explorviz/token/service/messaging/EventService.java
@@ -2,6 +2,9 @@ package net.explorviz.token.service.messaging;
 
 import net.explorviz.avro.TokenEvent;
 
+/**
+ * Interface for the emit event service.
+ */
 public interface EventService {
 
   void dispatch(TokenEvent event);
